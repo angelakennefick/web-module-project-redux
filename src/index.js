3925fa11c75933399ad70426984be461beefc2b7
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-06-08 17:02:48 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+ ); 
